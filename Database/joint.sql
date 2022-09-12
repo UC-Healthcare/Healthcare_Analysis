12813d47c347c94c7f14df5a6e1b02131583b7df
Kylie Hefner <kyliehefner@verizon.net>
2022-09-11 22:56:58 -0700

    Added comments

--- Database/joint.sql
+++ Database/joint.sql
@@ -1,3 +1,4 @@
+-- Create new table with the coordinates for each state
 CREATE TABLE statestable (
      id_ bigint  NOT NULL,
      state_ VARCHAR NOT NULL,
@@ -8,12 +9,13 @@ CREATE TABLE statestable (
 	PRIMARY KEY (id_)
 );
 
+-- Perform a left join between the state table and the survey dash data on the state id column
 SELECT st.state_, st.latitude, st.longitude, dd.*
 INTO dash_data_final
 FROM states as st
 LEFT JOIN survey_dash_data as dd
 ON st.id_ = dd.state ;
 
-
+-- Check join by selecting all rows from joined table where the state is Alabama (ID=1)
 SELECT * FROM dash_data_final as dd
 WHERE dd.id = 1
